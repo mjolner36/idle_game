7cc31a48bf1256bdcc549e1da5938fc31a18b8d4
Ruslan Rakhmanov <ruslan-3100@mail.ru>
2021-06-08 14:57:19 +0300

    button
    work on buttons

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public Sprite butt_on, butt_off;
+
+	void OnMouseDown()
+	{	
+		gameObject.GetComponent<Image>().sprite = butt_on;
+		
+	}
+
+	void OnMouseUp()
+	{
+		gameObject.GetComponent<Image>().sprite = butt_off;
+	}
+
+	void OnMouseUpAsButton()
+	{
+		switch (gameObject.name)
+		{
+			case "ButtonPlay":
+				SceneManager.LoadScene("Game");
+				break;
+			
+		}
+	}
+
+
+}
+

ebff45939ad4ef28d7d047d5f2b000dd47cc02a4
Ruslan Rakhmanov <ruslan-3100@mail.ru>
2021-06-08 23:15:20 +0300

    background

---
+++ Assets/Scripts/SpawnMonsters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnMonsters : MonoBehaviour
+{   
+    public GameObject Monster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Monster, new Vector2(Random.Range(-2.4f, 2.4f),Random.Range(-4.4f, 3f)), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

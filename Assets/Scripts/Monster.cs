20f300fb7c22105fd8ec6fcf920c4d39616eeeb5
Ruslan Rakhmanov <ruslan-3100@mail.ru>
2021-06-09 15:43:18 +0300

    sound

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,12 +6,18 @@ public class Monster : MonoBehaviour
 {
     public int Hp=1;
     public float speed;
+    AudioSource click;
+    
 
     void OnMouseUp()
     {
-        Destroy(gameObject);
+        
         Score.score++;
         GameObject.FindGameObjectWithTag("GameController").GetComponent<SpawnMonsters>().gameover--;
+        click = click = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
+        click.Play();
+        Destroy(gameObject);
+
 
     }
 }
